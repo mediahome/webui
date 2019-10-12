65ad30da8612a9a9fe73e690e0901ff52730991a
godcong <jumbycc@163.com>
2019-10-12 13:38:04 +0800

    update material

--- src/app/cluster/cluster.module.ts
+++ src/app/cluster/cluster.module.ts
@@ -6,15 +6,20 @@ import {ClusterComponent} from './cluster.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {LayoutModule} from '@angular/cdk/layout';
-import {
-  MatButtonModule, MatCardModule, MatCheckboxModule, MatExpansionModule, MatFormFieldControl, MatFormFieldModule,
-  MatGridListModule,
-  MatIconModule, MatInputModule,
-  MatListModule,
-  MatMenuModule, MatOptionModule, MatSelectModule,
-  MatSidenavModule,
-  MatToolbarModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatOptionModule } from '@angular/material/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import {FormsModule} from '@angular/forms';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {AddComponent} from './add/add.component';
